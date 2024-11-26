10fc44f0e2861e7a866d769d31196dce520f5834
Jonathan Gardner <jonathangardnermd@outlook.com>
2024-11-26 15:10:39 -0600

    minor change to models

--- jgib/models/models.py
+++ jgib/models/models.py
@@ -1,32 +1,40 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Any, List, Set, Optional
 from enum import Enum
 
 
-class Message(BaseModel):
-    text: str
+# class Message(BaseModel):
+#     text: str
 
-    def __str__(self):
-        return self.text
+#     def __str__(self):
+#         return self.text
 
 
 class InHouseApiModel(BaseModel):
-    msgType: str
-    data: Any
+    msgType: str = Field(
+        default_factory=lambda: None
+    )  # Default is None, will be set in __init__
+
+    def __init__(self, *args, **kwargs):
+        # Call the parent's __init__ method to properly handle initialization
+        super().__init__(*args, **kwargs)
+
+        # Dynamically set msgType to the subclass name
+        self.msgType = self.__class__.__name__
 
 
-class TickerDto(BaseModel):
+class TickerDto(InHouseApiModel):
     conId: int
     symbol: Any
     last: float
     # volume: Any
 
 
-class TickerList(BaseModel):
+class TickerList(InHouseApiModel):
     tickers: List[TickerDto]
 
 
-class QualifiedContractDto(BaseModel):
+class QualifiedContractDto(InHouseApiModel):
     conId: int
     symbol: str
     secType: str
@@ -36,7 +44,7 @@ class QualifiedContractDto(BaseModel):
     tickSize: Optional[float] = None
 
 
-class QualifiedContractList(BaseModel):
+class QualifiedContractList(InHouseApiModel):
     contracts: List[QualifiedContractDto]
 
 
@@ -47,5 +55,5 @@ class IbClientLifecycleEventType(Enum):
     CYCLE_COMPLETE = "cycleComplete"
 
 
-class IbClientLifecycleEventDto(BaseModel):
+class IbClientLifecycleEventDto(InHouseApiModel):
     event: IbClientLifecycleEventType
